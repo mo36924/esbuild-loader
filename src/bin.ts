1979aac333274953309316ea4207de32d2fe4b94
mo36924 <mo36924@users.noreply.github.com>
2021-06-28 07:56:29 +0900

    🐛

--- src/bin.ts
+++ src/bin.ts
@@ -1,20 +1,3 @@
-#!/usr/bin/env -S node --enable-source-maps
-import { build } from "esbuild";
-import { readFile } from "fs/promises";
-
-const data = await readFile("package.json", "utf-8");
-const { dependencies, devDependencies } = JSON.parse(data);
-const external = Object.keys({ ...dependencies, ...devDependencies });
-
-const result = await build({
-  entryPoints: [process.argv[2]],
-  bundle: true,
-  write: false,
-  platform: "node",
-  format: "esm",
-  external,
-  sourcemap: "inline",
-});
-
-const base64 = Buffer.from(result.outputFiles[0].contents).toString("base64");
-await import(`data:text/javascript;base64,${base64}`);
+#!/usr/bin/env -S node --enable-source-maps --experimental-loader @mo36924/esbuild-loader
+import { pathToFileURL } from "url";
+await import(pathToFileURL(process.argv[2]).href);
